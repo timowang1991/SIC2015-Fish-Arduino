51c6d68f476559f58142927ec852ec508c2e2c36
TeYanWu <killerw74@gmail.com>
2015-10-29 10:07:57 +1000

    1.resolve the BearingSlideMotor vol problem

--- BearingSlideMotor.cpp
+++ BearingSlideMotor.cpp
@@ -5,27 +5,27 @@ BearingSlideMotor::BearingSlideMotor(){
    pinMode(BEARING_SLIDE_MOTOR_PIN1, OUTPUT);
    currentPos = BEARING_SLIDE_TOTAL_LENGTH / 2;
    duration = 0;
-   analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_V);
-   analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_V); 
+   analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_GROUND_V);
+   analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_GROUND_V); 
 }
 
 void BearingSlideMotor::BearingSlideDown(){
     Serial.println("SlideDown");
 		duration = BEARING_SLIDE_STANDARD_DURATION * 1.5;
-		analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_V); 
-    analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_V);   
+		analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_HIGH_V); 
+    analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_GROUND_V);   
 }
 
 void BearingSlideMotor::BearingSlideUp(){
     Serial.println("SlideUP");
-		analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_V); 
-    analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_V); 
+		analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_GROUND_V); 
+    analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_HIGH_V); 
 }
 
 void BearingSlideMotor::BearingSlideStop(){
     Serial.println("SlideSTOP");
-    analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_V); 
-    analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_V); 
+    analogWrite(BEARING_SLIDE_MOTOR_PIN0, BEARING_SLIDE_GROUND_V); 
+    analogWrite(BEARING_SLIDE_MOTOR_PIN1, BEARING_SLIDE_GROUND_V); 
 }
 
 //
